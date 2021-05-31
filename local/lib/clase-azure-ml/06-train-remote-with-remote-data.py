06d41218422499e8c2cf78bed410265105e67924
Diana Milena Toro Arrieta <dmtoro@sura.com.co>
2021-05-30 23:31:43 -0500

    despliegue modelo  nube

--- local/lib/clase-azure-ml/06-train-remote-with-remote-data.py
+++ local/lib/clase-azure-ml/06-train-remote-with-remote-data.py
@@ -8,23 +8,22 @@ from azureml.core import Dataset
 if __name__ == "__main__":
     ws = Workspace.from_config(path='./.azureml',_file_name='config.json')
     datastore = ws.get_default_datastore()
-    dataset = Dataset.File.from_files(path=(datastore, 'datasets/cifar10'))
+    dataset = Dataset.File.from_files(path=(datastore, 'datasets/dataset_limpio.csv'))
 
-    experiment = Experiment(workspace=ws, name='day1-experiment-train-pytorch-data-remote')
+    experiment = Experiment(workspace=ws, name='day1-experiment-train-data-remote')
 
     config = ScriptRunConfig(
         source_directory='./src',
-        script='train-remote.py',
+        script='gestion_cartera.py',
         compute_target='cpu-cluster',
         arguments=[
-            '--data_path', dataset.as_named_input('input').as_mount(),
-            '--learning_rate', 0.003,
-            '--momentum', 0.92],
+            '--data_path', dataset.as_named_input('input').as_mount()
+            ],
     )
     # set up pytorch environment
     env = Environment.from_conda_specification(
-        name='pytorch-env',
-        file_path='./.azureml/pytorch-remote-env.yml'
+        name='amlproject-env',
+        file_path='./.azureml/sklearn-env-amlproject.yml'
     )
     config.run_config.environment = env
 
